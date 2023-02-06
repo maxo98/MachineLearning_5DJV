af6294267e8219d36d8335a1e459cf1ac756ebdd
Mute <40270424+Ced0@users.noreply.github.com>
2023-02-06 18:24:07 +0100

    Data colored and grey

---
+++ ML champions lol grey/ToGreyScale.py
@@ -0,0 +1,34 @@
+import subprocess
+import os.path
+import glob
+from datetime import datetime
+from PIL import Image
+
+
+def recurs(walk_in):
+    walk_result = os.walk(walk_in)
+    
+    for dir in walk_result:
+        
+        print("dir: " + dir[0])
+        
+        files = glob.glob(os.path.join(dir[0], '*.jpg'))
+        jpg = []
+
+        for x in files:
+            img = Image.open(x).convert('L')
+            img.save(x)
+
+            print(x)
+
+            #tmp = x.split("\\")
+            #jpg.append(tmp[len(tmp)-1])
+
+        #for file in jpg:
+
+            #print(file)
+
+            
+
+                    
+recurs(".")
